0473f86c526d8f8455a5e0cac8c1526f030f5681
champagnepaki <zion.dazz@outlook.com>
2025-06-20 03:20:47 +0100

    Initial commit with notebook and setup

---
+++ chapter2.py
@@ -0,0 +1,28 @@
+
+
+# Dont need sys call to assert python 3.7 as we are using python 3.12.0
+
+import sklearn
+
+from pathlib import Path 
+import pandas as pd
+import tarfile
+import urllib.request
+
+
+
+
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+    with tarfile.open(tarball_path) as housing_tarball:
+        housing_tarball.extractall(path="datasets")
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+housing = load_housing_data()
+housing.head()
+housing.info()
